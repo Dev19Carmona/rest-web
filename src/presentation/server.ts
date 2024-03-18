64362d2ca86f4ecc62750835749ef8e1dd35deab
Dev19Carmona <camilocr3@hotmail.com>
2024-03-18 14:56:58 -0500

    ----

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,21 +1,29 @@
 import express from 'express'
 import path from 'path'
-
+interface Options {
+  port: number
+  public_path?: string
+}
 export class Server {
   private app = express()
-  async start(){
+  private readonly port: number
+  private readonly publicPath: string
+  constructor(options: Options) {
+    const {port, public_path = 'public'} = options
+    this.port = port;
+    this.publicPath = public_path
+  }
+  async start() {
     //* Middlewares
     //* Public Folder
-    this.app.use(express.static('public'))
-    this.app.get('*', (req, res)=> {
+    this.app.use(express.static(this.publicPath))
+    this.app.get('*', (req, res) => {
       const indexPath = path.join(__dirname + '../../../public/index.html')
       res.sendFile(indexPath)
       return
     })
-    this.app.listen(3001, ()=>{
-      console.log('server running on port 3001');
-      
+    this.app.listen(this.port, () => {
+      console.log(`server running on port ${this.port}`)
     })
-    
   }
 }
