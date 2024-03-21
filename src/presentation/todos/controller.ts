51025bd45c418043b1d955fb3c9014d0c6710384
Dev19Carmona <camilocr3@hotmail.com>
2024-03-21 17:12:23 -0500

    gitignore updated

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
-import { prisma } from '../../data/postgresql'
+import { prisma } from '../../data/postgres'
 import { CreateTodoDto, UpdateTodoDto } from '../../domain/dtos'
 export class TodosController {
   constructor() {}
